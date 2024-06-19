efb0db6c929a753c03d777bd61b0d356e168d94b
DamsalapuriVamshi <vamshi21999@gmail.com>
2024-06-19 11:29:56 +0530

    Add files via upload

---
+++ Day 3 Intro To Node Js/app.js
@@ -0,0 +1,44 @@
+console.log("Description about Ninja bike")
+
+let zedx6R = {
+    model : "BS6",
+    type : "Liquid-cooled, 4-stroke In-Line Four",
+    Displacement :	"636 cm3",
+    Ignition :	"Digital",
+    Starting :	"Electric",
+    Transmission :	"6-speed, return",
+    Maximumpower :	"91.0 kW {124 PS} / 13,000 min-1",
+    MaximumpowerwithRamAir	: "95.2 kW {129 PS} / 13,000 min-1",
+    Maximumtorque :	"69.0 N·m {7.0 kgƒ·m} / 11,000 min-1",
+   FrameType :"	Perimeter, pressed-aluminium",
+    RakeTrail:"	23.5o / 101 mm",
+    TyreFront:	"120/70ZR17M/C (58W)",
+    TyreRear :	"180/55ZR17M/C (73W)",
+    Wheelbase :	"1,400 mm",
+    Groundclearance :	"130 mm",
+    Seatheight :	"830 mm",
+    Curbmass :	"198 kg (EUR/AUS/S.E. Asia (B1/B3)/IDN/THA/PHL)",
+    Fuelcapacity :	"17 litres",
+    OverallDimensions : "2,025 X 710 X 1,105 mm"
+}
+console.log(zedx6R)
+
+let zedx10R = [
+    "BS6",
+    "Liquid-cooled, 4-stroke In-Line Four",
+    "998 cm3",
+    "DOHC, 16 valves",
+    "Fuel injection: ø47 mm x 4 with dual injection",
+    "Unleaded petrol/RON95 Plus",
+    "Digital",
+    "Electric",
+    "Forced lubrication, wet sump with air-cooled oil cooler",
+    "6-speed, return",
+    "149.3 kW {203 PS} / 13,200 rpm",
+    "156.8 kW {213 PS} / 13,200 rpm",
+    "114.9 N·m {11.7 kgƒ·m} / 11,400 rpm",
+    "207 kg",
+    "17 litres",
+    "2,085 x 750 x 1,185 mm"
+]
+console.log(zedx10R)
